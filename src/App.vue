d412c364b46cfcda3a300058f4d4c26503b1b901
Melina <santillanzzwem@gmail.com>
2026-03-12 21:01:24 -0500

    chore: add vercel insights

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Layout from './shared/presentation/components/layout.vue';
 import { useI18n } from "vue-i18n";
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 
 const { t } = useI18n();
 </script>
 
 <template>
   <Layout />
+  <SpeedInsights />
 </template>
 
 <style>
